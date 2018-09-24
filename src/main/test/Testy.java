d3b16d8fb98d081b80902ab47de0c5026a3f47e2
codemonkey92 <42755902+codemonkey92@users.noreply.github.com>
2018-09-24 19:28:15 -0400

    working test for creating AutoReplyCommand

--- src/main/test/Testy.java
+++ src/main/test/Testy.java
@@ -1,9 +1,12 @@
-import org.junit.jupiter.api.Test;
 import emily.command.administrative.AutoReplyCommand;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class Testy {
     @Test
-    public void testSomething(){
+    public void testGetCommandEqualsAutoReply(){
         AutoReplyCommand sut = new AutoReplyCommand();
+        assertEquals("autoreply",sut.getCommand());
     }
 }
