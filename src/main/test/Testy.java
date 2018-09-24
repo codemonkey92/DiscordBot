698e3ccfbf1dcb71fa30d7bcc68d9c0dfe848e4f
codemonkey92 <42755902+codemonkey92@users.noreply.github.com>
2018-09-24 19:11:04 -0400

    testing a test and moved test directory

---
+++ src/main/test/Testy.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import emily.command.administrative.AutoReplyCommand;
+
+public class Testy {
+    @Test
+    public void testSomething(){
+        AutoReplyCommand sut = new AutoReplyCommand();
+    }
+}
--- test/Testy.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class Testy {
-    @Test
-    public void testSomething(){
-
-    }
-}
