2d90cf30cc1421d14ad5982a325596e41a096341
John Roark <jroarkpiano@gmail.com>
2018-10-22 17:55:28 -0400

    Added tests for multiple commands

--- src/main/test/Testy.java
+++ src/main/test/Testy.java
@@ -1,4 +1,9 @@
 import emily.command.administrative.AutoReplyCommand;
+import emily.command.fun.BlackJackCommand;
+import emily.command.fun.CatFactCommand;
+import emily.command.fun.EightballCommand;
+import emily.command.fun.EmojifyCommand;
+import emoji4j.Emoji;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,5 +13,29 @@ public class Testy {
     public void testGetCommandEqualsAutoReply(){
         AutoReplyCommand sut = new AutoReplyCommand();
         assertEquals("autoreply",sut.getCommand());
+    }
+
+    @Test
+    public void testGetCommandEqualsBlackjack(){
+        BlackJackCommand sut = new BlackJackCommand();
+        assertEquals("blackjack", sut.getCommand());
+    }
+
+    @Test
+    public void testGetCommandEqualsCatFact(){
+        CatFactCommand sut = new CatFactCommand();
+        assertEquals("catfact", sut.getCommand());
+    }
+
+    @Test
+    public void testGetCommandEquals8ball(){
+        EightballCommand sut = new EightballCommand();
+        assertEquals("8ball", sut.getCommand());
+    }
+
+    @Test
+    public void testGetCommandEqualsEmojify(){
+        EmojifyCommand sut = new EmojifyCommand();
+        assertEquals("emojify", sut.getCommand());
     }
 }
