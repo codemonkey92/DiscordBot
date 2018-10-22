9bd397c57c6abd0c5fc03b078f69072a43aa4985
BradShi <42755912+BradShi@users.noreply.github.com>
2018-10-22 18:03:38 -0400

    CommitingBradTest

--- src/main/test/Testy.java
+++ src/main/test/Testy.java
@@ -1,4 +1,5 @@
-import emily.command.administrative.AutoReplyCommand;
+import emily.command.administrative.*;
+import emily.games.blackjack.Blackjack;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,5 +9,23 @@ public class Testy {
     public void testGetCommandEqualsAutoReply(){
         AutoReplyCommand sut = new AutoReplyCommand();
         assertEquals("autoreply",sut.getCommand());
+    }
+    @Test
+    public void testGetCommandKickCommand()
+    {
+        KickCommand sut = new KickCommand();
+        assertEquals("kick",sut.getCommand());
+    }
+    @Test
+    public void testaGetCommandBanCommand()
+    {
+        BanCommand sut = new BanCommand();
+        assertEquals("ban", sut.getCommand());
+    }
+    @Test
+    public void testAutoReplyGetDescription()
+    {
+        AutoReplyCommand sut = new AutoReplyCommand();
+        assertEquals("regular expression Patterns where the bot auto-replies to. ", sut.getDescription());
     }
 }
