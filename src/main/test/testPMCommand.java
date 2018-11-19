d1197f5f9bec959e44d706623bf643c7687c01af
codemonkey92 <42755902+codemonkey92@users.noreply.github.com>
2018-11-19 11:53:10 -0500

    circleci integration / testPMCommand failure
    circleci integration / testPMCommand failure

--- src/main/test/testPMCommand.java
+++ src/main/test/testPMCommand.java
@@ -2,43 +2,55 @@ import emily.command.administrative.PMCommand;
 import emily.handler.SecurityHandler;
 import emily.main.DiscordBot;
 import emily.permission.SimpleRank;
+import emily.templates.Template;
 import emily.templates.Templates;
 import emily.util.DisUtil;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.TextChannel;
-import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.entities.impl.TextChannelImpl;
+import net.dv8tion.jda.core.entities.impl.UserImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class testPMCommand {
     PMCommand sut;
 
     @Mock
-    DiscordBot bot;
-    User author;
-    User target;
-    MessageChannel channel;
-    Message inputMessage;
+    DiscordBot bot = mock(DiscordBot.class, Mockito.RETURNS_DEEP_STUBS);
+    UserImpl author = mock(UserImpl.class, Mockito.RETURNS_DEEP_STUBS);
+    UserImpl target = mock(UserImpl.class, Mockito.RETURNS_DEEP_STUBS);
+    TextChannelImpl channel = mock(TextChannelImpl.class, Mockito.RETURNS_DEEP_STUBS);
+    Message inputMessage = mock(Message.class, Mockito.RETURNS_DEEP_STUBS);
+    SecurityHandler security = mock(SecurityHandler.class, Mockito.RETURNS_DEEP_STUBS);
 
     @Before
     public void init(){
         MockitoAnnotations.initMocks(this);
+        sut = new PMCommand();
     }
 
     @Test
     public void givenLessThanUser_whenExecute_thenNoPermissions(){
         String[] args = {"unpleasant_peasant","I","am","away","from","the","keyboard"};
 
-        when(bot.security.getSimpleRank(author,channel)).thenReturn(SimpleRank.BANNED_USER);
+        when(author.getIdLong()).thenReturn((long) 1);
 
-        when(DisUtil.findUser((TextChannel) channel, args[0])).thenReturn(target);
+        when(security.getSimpleRank(author,channel)).thenReturn(SimpleRank.BANNED_USER);
+
+        //when(DisUtil.findUser((TextChannel) channel, args[0])).thenReturn(target);
+
+        when(channel.getType()).thenReturn(ChannelType.TEXT);
+
+        when(((TextChannel) channel).getGuild().getIdLong()).thenReturn((long) 1);
 
         assertEquals(Templates.no_permission.formatGuild(channel),sut.execute(bot, args, channel, author, inputMessage));
     }
